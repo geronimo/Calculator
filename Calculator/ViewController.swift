291bb7ab3ff0aa2bd2e7628323adc1a747d5fe57
Geronimo De Abreu <geronimodeabreu@gmail.com>
2016-06-21 15:05:17 -0400

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Geronimo De Abreu on 2016-06-21.
+//  Copyright © 2016 Geronimo De Abreu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
